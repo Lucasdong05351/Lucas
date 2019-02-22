91744bc089e39574821a5fb248bbcb34881b47b1
Lucas <10442974@qq.com>
2019-02-22 22:09:22 +0800

    Default

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    @Test
+    public void testHelloWrold() {
+        System.out.println("HelloWorld!");
+        System.out.println("ffff");
+        System.out.println("ffff");
+        System.out.println("ffff");
+        System.out.println("ffff");
+        System.out.println("ffff");
+    }
+
+}
